f2b1f6eab7827713619b981f0263ad7c73a3fc90
Ary Davani <arydavani@gmail.com>
2017-06-20 18:18:52 -0400

    added user detail links

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ app.get('/directory', function(req, res) {
   res.render('index', data);
 })
 
+app.get('/directory/:id', function(req, res) {
+  var user;
+
+  for (var i = 0; i < data.users.length; i++) {
+    if (data.users[i].id == req.params.id){
+      user = data.users[i];
+    }
+  }
+  res.render('details', user);
+})
+
+
 
 app.listen(3000, function() {
   console.log('Listening...');
