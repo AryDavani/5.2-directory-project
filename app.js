7692245af00c3171f5951cda7959eca30b837845
Ary Davani <arydavani@gmail.com>
2017-06-20 16:08:54 -0400

    got elements to display on server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const app = express();
+const data = require('./data.js');
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+
+app.get('/directory', function(req, res) {
+
+  res.render('index', data);
+})
+
+
+app.listen(3000, function() {
+  console.log('Listening...');
+})
