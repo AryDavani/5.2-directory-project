184f9743f0e26f16da6e596ede1b2691bcd1b621
Ary Davani <arydavani@gmail.com>
2017-06-23 11:41:16 -0400

    styled details page

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
+const path = require('path');
 const mustacheExpress = require('mustache-express');
 const app = express();
 const data = require('./data.js');
 
-app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
-
 app.get('/directory', function(req, res) {
-
   res.render('index', data);
-})
+});
 
 app.get('/directory/:id', function(req, res) {
   var user;
@@ -23,10 +23,8 @@ app.get('/directory/:id', function(req, res) {
     }
   }
   res.render('details', user);
-})
-
-
+});
 
 app.listen(3000, function() {
   console.log('Listening...');
-})
+});
